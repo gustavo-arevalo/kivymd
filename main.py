49959dc810bb2ebc98712df87e4bdf0e299c06ed
claudiogustavoarevalo <59540211+claudiogustavoarevalo@users.noreply.github.com>
2021-03-21 23:34:31 -0300

    fin del domingo

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+
 class DashBoard(Screen):
     pass
+
+
 class FirstScreen(Screen):
     pass
 
+
 class MyApp(MDApp):
     def build(self):
-        return Builder.load_file("archivokivy.kv")
+        self.title = "MI primera app"
+        self.theme_cls.primary_pallet = "Red"
+        return Builder.load_file("main.kv")
 
 MyApp().run()
