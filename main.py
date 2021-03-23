ffd67bb898a7646144fe7b33035acfe66c70692b
claudiogustavoarevalo <59540211+claudiogustavoarevalo@users.noreply.github.com>
2021-03-23 00:38:06 -0300

    aca el pycharm me dijo que habia que commitear, y bue, le mande fruta

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+
+
+
 
 
 class DashBoard(Screen):
@@ -15,6 +21,5 @@ class MyApp(MDApp):
     def build(self):
         self.title = "MI primera app"
         self.theme_cls.primary_pallet = "Red"
-        return Builder.load_file("main.kv")
-
+        return Builder.load_file("archivo.kv")
 MyApp().run()
